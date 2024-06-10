7ef5c30906c9a9e4147ff86e73d3548fd3b7a8f4
FajarHamonangan <bengkelws@gmail.com>
2024-06-10 22:17:20 +0700

    query string

--- belajar-req-res.js
+++ belajar-req-res.js
@@ -1,5 +1,5 @@
 const http  = require('http')
-const fs    = require ('fs')
+const fs    = require('fs')
 const port  = 3000
 
 
@@ -7,31 +7,26 @@ let server = http.createServer( function(request, respon) {
     if (request.url == '/') {
         respon.writeHead(200, {'Content-Type': 'text/html'})
         fs.createReadStream('./view/utama.html').pipe(respon)
-        // fs.createdReadstream() => membaca file yang diinginkan
-        // pipe() => kirim sebagai apa, file yang sudah dibaca
-
-
-    } 
-    else if (request.url == '/beranda'){
+        // fs.createReadStream() => membaca file yg diinginkan
+        // pipe() => kirim sebagai apa, file yg sudah dibaca
+    }
+    else if (request.url == '/beranda') {
         respon.writeHead(200, {'Content-Type': 'text/html'})
         fs.createReadStream('./view/beranda.html').pipe(respon)
-
-
-    } 
-    else if (request.url == '/profil'){
-        respon.writeHead(200, {'Content-Type': 'text/html'})
-        fs.createReadStream('./view/profil.html').pipe(respon)
     }
-
+    else if (request.url == '/profil') {
+        respon.writeHead(200, {'Content-Type': 'text/html'})
+        respon.write('<h1>Profil Coach</h1>')
+        respon.end()
+    }
     else {
         respon.writeHead(404, {'Content-Type': 'text/html'})
-        respon.write('<h1>Putar Balik Om, Filenya Ngga Ada!</h1>')
+        respon.write('<h1>Putar balik om!, File yang dicari nggak ada!!</h1>')
         respon.end()
-
-
     }
 })
 
+
 server.listen(port, ()=>{
-    console.log(`server sudah on, buka http://localhost${port}`)
+    console.log(`server sudah on, buka http://localhost:${port}`)
 })
--- node-dasar.js
+++ node-dasar.js
@@ -1,22 +1,27 @@
 let nama = 'Fajar'
 let alamat = 'Jakarta'
-let gaji = 700000 *30 - 70000
+let gaji = 700000 * 30 - 70000
 
-
-
-
-
+// menggunakan modul http yg sudah disiapkan oleh node.js
+// modul http berfungsi untuk
+// membuat komputer kita bisa diakses dari luar
+// (dengan menggunakan internet)
+// localhost adalah replika (dummy) dari situs web asli tapi hanya jalan di komputer kita
+// untuk keperluan pengembangan/pembuatan aplikasi
 const http = require('http')
 
-let server = http.createServer((req,res) => {
-    // mendefinisikan status code dan tipe konten yang ingin diberikan ke user
+let server = http.createServer((req, res) => {
+    // mendefinisikan status code dan tipe konten yg ingin diberikan ke user
+    res.writeHead(200, { 'Content-Type': 'text/html' })
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
     // isi konten
-    
-    res.end('<h1>'+nama+'</h1>' + ' ' + alamat + ' ' + gaji)
+    res.end('<h1>' + nama + '</h1>' + ' ' + alamat + ' ' + gajian + 'm')
 })
 
+
+// konfigurasi port yg ingin digunakan
+// karena port default (80) sudah dipakai oleh software lain
+// jadi kita pakai port yg free
 server.listen(3000, ()=>{
-    console.log('server sudah siap')
+    console.log('server sudah siap jalan')
 })
